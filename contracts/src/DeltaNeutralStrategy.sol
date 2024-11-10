7d7bade13b9708b3de25432ef02e4c6553e7195a
Anturui <anturui@mail.ru>
2024-11-10 16:05:33 +0300

    update

--- contracts/src/DeltaNeutralStrategy.sol
+++ contracts/src/DeltaNeutralStrategy.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-import "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";    
-import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";          
+import "contracts/lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";     
+import "contracts/lib/openzeppelin-contracts/contracts/access/Ownable.sol";          
 
 contract DeltaNeutralStrategy is Ownable {
     // Структура для хранения позиции
